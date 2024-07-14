d99bf5cfe152d30eff1080f2cc3c79c994c5e5c2
pratapcode <yadavsugam347@gmail.com>
2024-07-15 00:36:58 +0545

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+
+function rollDice(){
+    const diceNumber = document.getElementById("diceNumber").value;
+    const diceResult = document.getElementById("diceResult");
+    const diceImage = document.getElementById("diceImage");
+    const values =[];
+    const image =[];
+
+    
+    for(let i=0; i < diceNumber; i++){
+        const value = Math.floor(Math.random() * 6)+1;
+        values.push(value);
+        // image.push('<img src="dice_roller/1.png" alt="Dice Image">');
+        image.push('<img src ="dice_roller/${value}.png" alt="Dice Image">');
+    
+    }
+    diceResult.textContent = `Dice : ${values.join(`, `)}`;
+    diceImage.innerHTML = image.join(``);
+}
